deea920031855052319294b0bf2ea9c9ba5d9cef
Min Gi Han <hk335078@naver.com>
2021-08-04 17:35:44 +0900

    Update 1018.py

--- Baekjoon/1018.py
+++ Baekjoon/1018.py
@@ -2,30 +2,25 @@ def BW_board(y, x, chess_board):
     answer = ["WBWBWBWB", "BWBWBWBW"]
     before_color = chess_board[y][x]
     a_count, b_count = 0, 0
-    for i_a in range(y, y + 8):
-        for j_a in range(x, x + 8):
-            if (i_a - y) % 2 == 0:
-                if answer[0][j_a - x] != chess_board[i_a][j_a]:
+    for i in range(y, y + 8):
+        for j in range(x, x + 8):
+            if (i - y) % 2 == 0:
+                if answer[0][j - x] != chess_board[i][j]:
                     a_count += 1
-            elif (i_a - y) % 2 != 0:
-                if answer[1][j_a - x] != chess_board[i_a][j_a]:
-                    a_count += 1
-
-    for i_b in range(y, y + 8):
-        for j_b in range(x, x + 8):
-            if (i_b - y) % 2 == 0:
-                if answer[1][j_b - x] != chess_board[i_b][j_b]:
+                if answer[1][j - x] != chess_board[i][j]:
                     b_count += 1
-            elif (i_b - y) % 2 != 0:
-                if answer[0][j_b - x] != chess_board[i_b][j_b]:
+            elif (i - y) % 2 != 0:
+                if answer[1][j - x] != chess_board[i][j]:
+                    a_count += 1
+                if answer[0][j - x] != chess_board[i][j]:
                     b_count += 1
     return min(a_count, b_count)
 
 
 N, M = map(int, input().split())
 chess_board = [0 for _ in range(N)]
-for i in range(N):
-    chess_board[i] = list(map(str, input()))
+for i_chess in range(N):
+    chess_board[i_chess] = list(map(str, input()))
 min_count = 64
 for y in range(N - 7):
     for x in range(M - 7):
