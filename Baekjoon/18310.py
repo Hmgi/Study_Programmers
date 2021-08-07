2ee821be08a26a92cd784314acb5752e6a1a55f3
Hmgi <hk335078@naver.com>
2021-08-07 19:53:52 +0900

    refactoring

--- Baekjoon/18310.py
+++ Baekjoon/18310.py
@@ -1,15 +1,4 @@
 n = int(input())
 a = list(map(int, input().split()))
 a.sort()
-sum = 0
-min = 0
-
-for i in range(a[0], a[len(a)-1] + 1):
-    for j in range(n):
-       sum += (i - a[j])
-
-    if min > sum:
-        min = sum
-        minHouse = i
-
-print(minHouse)
+print(a[(n - 1) // 2])
