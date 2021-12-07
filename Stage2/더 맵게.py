210e16cfa9b979ef9859925800546f187bf8ce61
Hmgi <hk335078@naver.com>
2021-12-07 15:06:03 +0900

    1207
    update code

--- Stage2/더 맵게.py
+++ Stage2/더 맵게.py
@@ -2,21 +2,16 @@ import heapq
 def solution(scoville, K):
     answer = 0
 
-    heap = []
-    for i in scoville:
-        heapq.heappush(heap, i)
+    heapq.heapify(scoville)
 
-    while True:
-        if len(heap) >= 2:
-            heapq.heappush(heap, heapq.heappop(heap) + (heapq.heappop(heap) * 2))
-        else:
-            if heap[0] < K:
-                return -1
+    while len(scoville) >= 2: # 리스트 길이는 최소 2개여야 한다.
+        if scoville[0] >= K:
+            return answer
         answer += 1
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        heapq.heappush(scoville, a + (b * 2))
+    if scoville[0] < K: # 계산이 완료 되었을 때 나머지 1개가 K보다 작으면 -1 출력
+        return -1
 
-        if heap[0] >= K:
-            break
     return answer
-
-
-print(solution([1, 2, 3, 9, 10, 12], 7))
