a63b375166bde8bace2a4281ae217a613776fa61
Min Gi Han <hk335078@naver.com>
2022-05-26 09:38:20 +0900

    2022-05-26
    백준 1343번 자바 문제풀이

---
+++ Baekjoon/Main_1343.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main_1343 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String arr = "";
+
+        String input = sc.next();
+
+        input = input.replaceAll("XXXX", "AAAA");
+        input = input.replaceAll("XX", "BB");
+
+        if(input.contains("X")){
+            System.out.println("-1");
+        }
+        else{
+            System.out.println(input);
+        }
+
+    }
+}
