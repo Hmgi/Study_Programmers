8d74764eb6c4a39928295d96dd815a7ef0163c1b
Min Gi Han <hk335078@naver.com>
2022-05-28 15:10:49 +0900

    refactoring

--- Baekjoon/Main_1343.java
+++ Baekjoon/Main_1343.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main_1343 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String arr = "";
 
         String input = sc.next();
 
