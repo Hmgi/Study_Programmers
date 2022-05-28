a4a0a65f0d77f29cd39685af761b250e251b1454
Min Gi Han <hk335078@naver.com>
2022-05-28 15:10:13 +0900

    update class name
    update class name

--- Baekjoon/Main_1427.java
+++ Baekjoon/Main_1427.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Main_1343 {
+public class Main_1427 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
