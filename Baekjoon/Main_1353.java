0af538939b5b6219c05f9658237ec5b18519a8fe
Min Gi Han <hk335078@naver.com>
2022-06-22 16:51:44 +0900

    [INIT] ADD MAIN_1353
    solved 1353
    on 2022-06-22

---
+++ Baekjoon/Main_1353.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main_1353 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int s = sc.nextInt();
+        int p = sc.nextInt();
+
+        if(s==p){
+            System.out.println("1");
+            return;
+        }
+
+        double prev = -1;
+        for(int i = 2; ; i++){
+            double curr = Math.pow(1.0 * s / i, i);
+            if(prev > curr){
+                System.out.println("-1");
+                return;
+
+            }
+            if(p <= curr){
+                System.out.println(i);
+                return;
+            }
+            prev = curr;
+        }
+
+    }    
+}
