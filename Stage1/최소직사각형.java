88573d8ead7bcf0f5652142698f07096487ce9ba
Min Gi Han <hk335078@naver.com>
2022-07-05 10:23:15 +0900

    [ADD] 최소직사각형.java
    [ADD] 최소직사각형.java

---
+++ Stage1/최소직사각형.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] sizes){
+        int min = 1;
+        int max = 1;
+        int min_temp, max_temp;
+        
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][0] > sizes[i][1]) {
+                max_temp = sizes[i][0];
+                min_temp = sizes[i][1];
+            }
+            else{
+                max_temp = sizes[i][1];
+                min_temp = sizes[i][0];
+            }
+
+            if(max < max_temp) max = max_temp;
+            if(min < min_temp) min = min_temp;
+        }
+
+
+        return max * min;
+    }
+}
+
+public class 최소직사각형{
+    public static void main(String[] args){
+        Solution test = new Solution();
+        int arr[][] = {{60,50},{30,70},{60, 30},{80, 40}};
+        System.out.println(test.solution(arr));
+    }
+}
